e0cdbd951bfe14b16fbd19838373b3cdae5affcd
Colin Sheppard <colinsheppard10@gmail.com>
2016-02-21 21:49:11 -0600

    MAC

--- TCPSelectServer.cpp
+++ TCPSelectServer.cpp
@@ -96,7 +96,7 @@ void android(char* input){
     char *secondHalf = "');";
     
     strcat(buffer, input);
-    buffer[strlen(buffer)] = 0;
+    buffer[strlen(buffer)-1] = 0;
     strcat(buffer, secondHalf);
     
     cout << "from finished: "<< buffer << endl;
