dafd19c207c56ef70b760a38b47c3f720ce40a06
Colin Sheppard <colinsheppard10@gmail.com>
2016-02-22 18:30:07 -0600

    MAC

---
+++ ShineRise/app/src/main/java/com/example/colin/shinerise/EditorActivity.java
@@ -0,0 +1,26 @@
+package com.example.colin.shinerise;
+
+import android.database.Cursor;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.CursorAdapter;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+
+public class EditorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_editor);
+
+        Cursor cursor = getContentResolver().query(AlarmsProvider.CONTENT_URI, DBOpenHelper.ALL_COLUMNS, null, null, null, null);
+        String[] from = {DBOpenHelper.ALARM_TEXT};
+        int[] to = {android.R.id.text1};
+
+        CursorAdapter cursorAdapter = new SimpleCursorAdapter(this, android.R.layout.simple_list_item_1, cursor, from, to, 0);
+
+        ListView list = (ListView) findViewById(android.R.id.list);
+        list.setAdapter(cursorAdapter);
+    }
+}
