8d763d887eb6db85377646d875a98515b17dd55a
Colin <colinsheppard10@gmail.com>
2016-04-04 10:03:42 -0500

    ok

--- TCPSelectServer.cpp
+++ TCPSelectServer.cpp
@@ -8,7 +8,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
-//#include <header.h>
+#include <header.h>
 
 using std::cout;
 using std::endl;
@@ -164,7 +164,7 @@ void android(char* input){
     }
     
     cout << "from android finished: "<< buffer << endl;
-    //   access_database(buffer,dbBuffer);
+    access_database(buffer,dbBuffer);
     
 }
 void esp(char* dbBuffer){
@@ -190,7 +190,7 @@ void esp(char* dbBuffer){
     strcat(queryBuffer, buffer);
     strcat(queryBuffer, secondHalf);
     cout <<"prepared query from esp(): " << queryBuffer << endl;
-    //    access_database(queryBuffer, dbBuffer);
+    access_database(queryBuffer, dbBuffer);
     cout <<"dbBuffer from esp(): " << dbBuffer << endl << endl;
     
 }
